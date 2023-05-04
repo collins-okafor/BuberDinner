e83eaf033030ae8186a87b37f53ea76e8d62512d
Collins Okafor <collinszurum@gmail.com>
2023-05-04 16:09:25 +0100

    jwt token generatored created in infrastructure

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -2,19 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BuberDinner.Application.Common.Interfaces.Authentication;
 
 namespace BuberDinner.Application.Services.Authentication
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+        public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
+        {
+            _jwtTokenGenerator = jwtTokenGenerator;
+        }
         public AuthenticationResult Register(string firstName, string lastName, string email, string password)
         {
+            // Check if user exist
+
+            // Create user (generate unique ID)
+
+            // Create JWT token
+            Guid userId = Guid.NewGuid();
+
+            var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+
             return new AuthenticationResult(
                 Guid.NewGuid(),
                 firstName,
                 lastName,
                 email,
-                "Token");
+                token);
         }
 
         public AuthenticationResult Login(string email, string password)
