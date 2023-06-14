d1a847b36cc680bcc24db284093befe27dd81bfc
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-06-14 09:59:35 +0100

    Global Error Handling: Custom problem details factory

--- BuberDinner.Api/Controllers/ErrorsController.cs
+++ BuberDinner.Api/Controllers/ErrorsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BuberDinner.Api.Controllers
@@ -11,6 +12,7 @@ namespace BuberDinner.Api.Controllers
         [Route("/error")]
         public IActionResult Error()
         {
+            Exception? exception = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
             return Problem();
 
         }
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,8 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filters;
 using BuberDinner.Application; 
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -11,6 +13,8 @@ var builder = WebApplication.CreateBuilder(args);
     // builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
 
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 
