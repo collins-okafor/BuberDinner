007754607e8aeb5ea2ccec05932ab2fd9873c96e
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-06-18 08:07:28 +0100

    query handler

--- BuberDinner.Application/DependencyInjection.cs
+++ BuberDinner.Application/DependencyInjection.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BuberDinner.Application.Services.Authentication.Commands;
-using BuberDinner.Application.Services.Authentication.Queries;
 using Microsoft.Extensions.DependencyInjection;
 
 
@@ -13,9 +11,7 @@ namespace BuberDinner.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddScoped<IAuthenticationQueryService, AuthenticationQueryService>();
-            services.AddScoped<IAuthenticationCommandService, AuthenticationCommandService>();
-
+        
             return services;
         }
     }
