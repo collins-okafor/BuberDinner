97b7db0716d1dc67e177f4b6d8dc24d81ec9a8ef
Collins Okafor <69325061+collins-okafor@users.noreply.github.com>
2023-07-06 15:05:24 +0100

    validateRegistercommand handler

--- BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
+++ BuberDinner.Application/Common/Behaviors/ValidationBehavior.cs
@@ -5,16 +5,34 @@ using System.Threading.Tasks;
 using BuberDinner.Application.Authentication.Commands.Register;
 using BuberDinner.Application.Services.Authentication.Common;
 using ErrorOr;
+using FluentValidation;
+using FluentValidation.Results;
 using MediatR;
 
 namespace BuberDinner.Application.Common.Behaviors
 {
     public class ValidateRegisterCommandBehavior : IPipelineBehavior<RegisterCommand, ErrorOr<AuthenticationResult>>
     {
-        public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand request, RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next, CancellationToken cancellationToken)
+        private readonly IValidator<RegisterCommand> _validator;
+        public ValidateRegisterCommandBehavior(IValidator<RegisterCommand> validator)
         {
-            var  result = await next();
-            return result;
+            _validator = validator;
+            
+        }
+        public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand request,
+            RequestHandlerDelegate<ErrorOr<AuthenticationResult>> next,
+            CancellationToken cancellationToken)
+        {
+            var validationResult = await _validator.ValidateAsync(request, cancellationToken);
+            if(validationResult.IsValid)
+            {
+                return await next();
+            }
+            var errors = validationResult.Errors
+                .Select(x => Error.Validation(x.PropertyName, x.ErrorMessage))
+                .ToList();
+           
+            return errors;
         }
     }
 }
